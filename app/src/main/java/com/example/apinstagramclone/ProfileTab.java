df1eeadea15c1c257581d476e07ac1be4eb36cea
ledungcobra <ledungcobra@gmail.com>
2020-02-25 01:34:05 +0700

    Implement user tab

--- app/src/main/java/com/example/apinstagramclone/ProfileTab.java
+++ app/src/main/java/com/example/apinstagramclone/ProfileTab.java
@@ -5,14 +5,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileTab extends Fragment {
+    private EditText edtName,edtBio,edtProfession,edtHobbies,
+            edtFavoriteSport;
+
+    private Button btnUpdateInfo;
+
+
 
 
     public ProfileTab() {
@@ -24,7 +37,74 @@ public class ProfileTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        edtName = view.findViewById(R.id.edtProfileName);
+        edtBio = view.findViewById(R.id.edtProfileBio);
+        edtFavoriteSport = view.findViewById(R.id.edtProfileFavoriteSport);
+        edtProfession = view.findViewById(R.id.edtProfileProfession);
+        edtHobbies = view.findViewById(R.id.edtProfileHobbies);
+
+        final ParseUser parseUser = ParseUser.getCurrentUser();
+
+        setContentToEditText(edtBio,"profileBio");
+        setContentToEditText(edtName,"profileName");
+        setContentToEditText(edtProfession,"profileProfession");
+        setContentToEditText(edtHobbies,"profileHobbies");
+        setContentToEditText(edtFavoriteSport,"profileFavSport");
+
+
+
+        btnUpdateInfo = view.findViewById(R.id.btnUpdateInfo);
+
+
+        btnUpdateInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                parseUser.put("profileName",edtName.getText()+"");
+                parseUser.put("profileBio",edtBio.getText()+"");
+                parseUser.put("profileProfession",edtProfession.getText()+"");
+                parseUser.put("profileHobbies",edtHobbies.getText()+"");
+                parseUser.put("profileFavSport",edtFavoriteSport.getText()+"");
+
+
+
+                parseUser.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e == null){
+                            FancyToast.makeText(getContext(),
+                                    "Info updated",FancyToast.LENGTH_SHORT,FancyToast.SUCCESS,false).show();
+
+                        }else{
+                            FancyToast.makeText(getContext(),
+                                    e.getMessage(),FancyToast.LENGTH_SHORT
+                                    ,FancyToast.ERROR,false).show();
+
+
+                        }
+                    }
+                });
+
+            }
+        });
+
+        return view;
+
     }
+    private void setContentToEditText(EditText edt,String columnName){
+
+        final ParseUser parseUser = ParseUser.getCurrentUser();
+        if(parseUser.get(columnName)!=null) {
+            edt.setText(parseUser.get(columnName).toString());
+        }else{
+
+            edt.setText("");
+
+        }
+
+    }
+
 
 }
--- app/src/main/java/com/example/apinstagramclone/UserTab.java
+++ app/src/main/java/com/example/apinstagramclone/UserTab.java
@@ -2,17 +2,36 @@ package com.example.apinstagramclone;
 
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class UserTab extends Fragment {
+
+    private ListView listView;
+    private ArrayList arrayList;
+    private ArrayAdapter arrayAdapter;
+
+
+
 
 
     public UserTab() {
@@ -24,7 +43,71 @@ public class UserTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_tab, container, false);
+        View v =  inflater.inflate(R.layout.fragment_user_tab, container, false);
+
+        listView  = v.findViewById(R.id.listView);
+
+        arrayList = new ArrayList();
+        arrayAdapter = new ArrayAdapter(getContext(),android.R.layout.simple_list_item_1,arrayList);
+
+        final TextView txtLoadingUsers = v.findViewById(R.id.txtLoadingUsers);
+
+
+
+        ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+        parseQuery.whereNotEqualTo("username",ParseUser.getCurrentUser().getUsername());
+        parseQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+
+
+                if(e == null){
+                    if(objects.size()>0){
+                        for(ParseUser user:objects){
+
+                            arrayList.add(user.getUsername());
+
+
+
+                        }
+                        listView.setAdapter(arrayAdapter);
+                        txtLoadingUsers.animate().setDuration(1500).alpha(0.0f);
+                        final CountDownTimer countDownTimer = new CountDownTimer(1500,500){
+
+                            @Override
+                            public void onTick(long millisUntilFinished) {
+
+
+
+                            }
+
+                            @Override
+                            public void onFinish() {
+                                txtLoadingUsers.setVisibility(View.INVISIBLE);
+                                listView.setVisibility(View.VISIBLE);
+                                listView.setRotation(180);
+
+                                listView.setAlpha(0f);
+
+                                listView.animate().setDuration(1000).alpha(1.0f).rotation(360);
+
+
+
+
+
+                            }
+                        }.start();
+
+
+                    }
+                }
+            }
+        });
+
+
+
+        return v;
+
     }
 
 }
